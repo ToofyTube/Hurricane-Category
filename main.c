cb23b6c0832b3a4f53551b757b6c5583daec7287
Mostafa Alutby <78908377+ToofyTube@users.noreply.github.com>
2024-04-08 01:31:49 +0000

    FIX

--- main.c
+++ main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
-int main() {
-    char x[] = "152 mph";
-    char y[] = "97.913 mph";
-    char z[] = "70.55 mph";
-    char j[] = "175.99 mph";
-  char input[9999];
-  printf("Enter wind speed in MPH:\n");
-  fgets(input, sizeof(input), stdin);
-  input[strcspn(input, "\n")] = 0; 
-  if (strcmp(input, x) == 0) {
-      printf("Category Four Hurricane\n");
-  } else if (strcmp(input, y) == 0) {
-      printf("Category Two Hurricane\n");
-  } else if (strcmp(input, z) == 0) {
-      printf("Not a Hurricane\n");
-  } else if (strcmp(input, j) == 0) {
-      printf("Category Five Hurricane\n");
-  } else {
-      printf("Nothing Was Found\n");
-  }
+int isPerfect(int num) {
+    printf("Enter hurricane speed: ");
+    scanf("%d", &num);
+
+    if (num >= 157) {
+        printf("Category 5");
+    } else if (num >= 130) {
+        printf("Category 4");
+    } else if (num >= 111) {
+        printf("Category 3");
+    } else if (num >= 96) {
+        printf("Category 2");
+    } else if (num >= 74) {
+        printf("Category 1");
+    } else {
+        printf("Not a hurricane");
+    }
+
     return 0;
 }
+
+int main() {
+    isPerfect(0); // Call isPerfect function with initial value 0
+    return 0;
+}
+
